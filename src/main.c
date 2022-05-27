2bfedc3c5973d018ffd8baa8128160c8c0c5448f
Mithras Kuipers <mithraskuipers@gmail.com>
2022-05-27 10:13:20 +0200

    update

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/25 23:09:34 by mikuiper      #+#    #+#                 */
-/*   Updated: 2022/05/27 09:54:15 by mikuiper      ########   odam.nl         */
+/*   Updated: 2022/05/27 10:13:08 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,49 @@ void	check_input(t_env *env)
 	if (ps_isordered(&env->stack_a))
 		exit(2);
 }
+
+void	findNum(t_env *env, int num, int buf)
+{
+	int	x;
+	int	y;
+	int	atoi;
+
+	while (1)
+	{
+		atoi = ft_atoi(env->stack_a->content);
+		if (num == INTMIN)
+			return ;
+		if ((num - buf) <= atoi && (num + buf) >= atoi)
+			return ;
+		x = get_idx_for_value(&env->stack_a, num)
+		//x = getIndex(v->a, num);
+		//y = ft_node_size(v->a) / 2;
+		y = n_nodes(&env->stack_a) / 2;
+		if (x < y)
+		{
+			rotate_a(&env->stack_a);
+			//caller(v, RA);
+		}
+		else
+		{
+			rrotate_a(&env->stack_a);
+			//caller(v, RRA);
+		}
+	}
+}
+
+void	bob(t_env *env, int nbr)
+{
+	if (ps_isordered(&env->stack_a))
+	{
+		return;
+	}
+	while (n_nodes(&env->stack_a) > 1)
+	{
+
+	}
+}
+
 
 int	main(int argc, char **argv)
 {

bb9d5004c0cfbf0409cc5cc0b680f445b7d2027c
Mithras Kuipers <mkuipers@192.168.2.3>
2022-06-07 12:30:36 +0200

    almost leak free

--- src/commands/cmd_push.c
+++ src/commands/cmd_push.c
@@ -6,50 +6,34 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/05 19:37:07 by mikuiper      #+#    #+#                 */
-/*   Updated: 2022/06/06 19:58:35 by mikuiper      ########   odam.nl         */
+/*   Updated: 2022/06/07 12:25:20 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-
 void	push_a(t_env *env)
 {
-	t_stack	*tmp;
+	t_stack	*buff;
 
-	if (n_nodes(&env->stack_b) > 0)
-	{
-		tmp = popnode_front(&env->stack_b);
-		insert_node(&env->stack_a, tmp, 0);
-		write(1, "pa\n", 3);
-	}
-}
-
-void	push_b(t_env *env)
-{
-	t_stack	*tmp;
-
-	if (n_nodes(&env->stack_a) > 0)
-	{
-		tmp = popnode_front(&env->stack_a);
-		insert_node(&env->stack_b, tmp, 0);
-		write(1, "pb\n", 3);
-	}
-}
-
-
-
-/*
-void	push_a(t_env *env)
-{
-
+	if (n_nodes(&env->stack_b) == 0) // node size
+		return ;
+	buff = env->stack_b;
+	env->stack_b = env->stack_b->next;
+	buff->next = env->stack_a;
+	env->stack_a = buff;
 	write(1, "pa\n", 3);
 }
 
-
 void	push_b(t_env *env)
 {
+	t_stack	*buff;
 
+	if (n_nodes(&env->stack_a) == 0) // node size
+		return ;
+	buff = env->stack_a;
+	env->stack_a = env->stack_a->next;
+	buff->next = env->stack_b;
+	env->stack_b = buff;
 	write(1, "pb\n", 3);
 }
-*/
--- src/commands/cmd_rotate.c
+++ src/commands/cmd_rotate.c
@@ -6,7 +6,7 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/05 16:00:03 by mikuiper      #+#    #+#                 */
-/*   Updated: 2022/05/31 11:36:02 by mikuiper      ########   odam.nl         */
+/*   Updated: 2022/06/07 12:20:37 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,66 @@
 
 void	rotate_a(t_env *env)
 {
-	t_stack	*tmp;
+	t_stack	*first_list;
+	t_stack	*rotate_list;
+	t_stack	*last_list;
 
-	if (n_nodes(&env->stack_a) > 0)
-	{
-		tmp = popnode_front(&env->stack_a);
-		addnode_end(&env->stack_a, tmp);
-		write(1, "ra\n", 3);
-	}
+	if (env->stack_a == NULL || env->stack_a->next == NULL)
+		return ;
+	rotate_list = env->stack_a;
+	first_list = env->stack_a->next;
+	last_list = env->stack_a;
+	while (last_list->next != NULL)
+		last_list = last_list->next;
+	rotate_list->next = NULL;
+	last_list->next = rotate_list;
+	env->stack_a = first_list;
+	write(1, "ra\n", 3);
 }
 
 void	rotate_b(t_env *env)
 {
-	t_stack	*tmp;
+	t_stack	*first_list;
+	t_stack	*rotate_list;
+	t_stack	*last_list;
 
-	if (n_nodes(&env->stack_b) > 0)
-	{
-		tmp = popnode_front(&env->stack_b);
-		addnode_end(&env->stack_b, tmp);
-		write(1, "rb\n", 3);
-	}
+	if (env->stack_b == NULL || env->stack_b->next == NULL)
+		return ;
+	rotate_list = env->stack_b;
+	first_list = env->stack_b->next;
+	last_list = env->stack_b;
+	while (last_list->next != NULL)
+		last_list = last_list->next;
+	rotate_list->next = NULL;
+	last_list->next = rotate_list;
+	env->stack_b = first_list;
+	write(1, "rb\n", 3);
 }
 
 void	rotate_s(t_env *env)
 {
-	t_stack	*tmp;
+	t_stack	*first_list;
+	t_stack	*rotate_list;
+	t_stack	*last_list;
 
-	if ((n_nodes(&env->stack_a) > 0) && (n_nodes(&env->stack_b) > 0))
-	{
-		tmp = popnode_front(&env->stack_a);
-		addnode_end(&env->stack_a, tmp);
-		tmp = popnode_front(&env->stack_b);
-		addnode_end(&env->stack_b, tmp);
-		write(1, "rr\n", 3);
-	}
+	if (env->stack_a == NULL || env->stack_a->next == NULL || \
+	env->stack_b == NULL || env->stack_b->next == NULL)
+		return ;
+	rotate_list = env->stack_a;
+	first_list = env->stack_a->next;
+	last_list = env->stack_a;
+	while (last_list->next != NULL)
+		last_list = last_list->next;
+	rotate_list->next = NULL;
+	last_list->next = rotate_list;
+	env->stack_a = first_list;
+	rotate_list = env->stack_b;
+	first_list = env->stack_b->next;
+	last_list = env->stack_b;
+	while (last_list->next != NULL)
+		last_list = last_list->next;
+	rotate_list->next = NULL;
+	last_list->next = rotate_list;
+	env->stack_b = first_list;
+	write(1, "rr\n", 3);
 }

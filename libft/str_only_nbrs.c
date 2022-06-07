dde98d397dc96c26ca4600f81e3d69809b6cc3e9
Mithras Kuipers <mithraskuipers@gmail.com>
2022-06-07 16:31:08 +0200

    resolved all memory leaks

--- libft/str_only_nbrs.c
+++ libft/str_only_nbrs.c
@@ -6,7 +6,7 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/26 15:22:20 by mikuiper      #+#    #+#                 */
-/*   Updated: 2022/06/02 11:43:31 by mikuiper      ########   odam.nl         */
+/*   Updated: 2022/06/07 16:30:59 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	str_only_nbrs(char *s)
 	int	i;
 
 	i = 0;
-	if (s[i] == '-')
+	if (s[i] == '-' && s[i + 1])
 		i++;
 	else if ((ft_isdigit(s[i]) != 1))
 		return (0);
